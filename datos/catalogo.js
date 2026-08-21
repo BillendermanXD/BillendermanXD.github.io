1de567e5fee8be8f177f2ab2758d544cebb68abf
BillendermanXD <147894886+BillendermanXD@users.noreply.github.com>
2026-08-21 00:30:32 -0400

    Actualizar contenido desde el gestor

--- datos/catalogo.js
+++ datos/catalogo.js
@@ -1,20 +1,12 @@
 window.hakinovCatalogo = {
     "juegos": [
         {
-            "nombre": "casa",
-            "descripcion": "",
-            "url": "https://youtube.com",
-            "tipoUrl": "interna",
-            "imagen": "Resorce/img/atlas.jpeg",
-            "pagina": "publicaciones/juegos-casa-1.html"
-        },
-        {
             "nombre": "casa2",
             "descripcion": "dadadadada",
             "url": "https://pornhub.com",
             "tipoUrl": "externa",
             "imagen": "Resorce/img/Logo.jpeg",
-            "pagina": "publicaciones/juegos-casa2-2.html"
+            "pagina": "publicaciones/juegos-casa2-1.html"
         }
     ],
     "clientes": []
