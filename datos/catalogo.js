18e9e7b61d79eb5d46e6cc4be3510eb8064152c3
BillendermanXD <147894886+BillendermanXD@users.noreply.github.com>
2026-08-22 04:30:23 -0400

    gestor-0.4

--- datos/catalogo.js
+++ datos/catalogo.js
@@ -11,12 +11,12 @@ window.hakinovCatalogo = {
     ],
     "clientes": [
         {
-            "nombre": "Lunar Cleint",
+            "nombre": "Lunar Client",
             "descripcion": "Uno de los clientes de pvp por excelencia mas confiables y viables de la ultima decada, tambien contempla una funcion diirecta para usar Badlain Client desde que Lunar asimilo sus funciones. Ahora es posible usarlo en su apartado de versiones en el launcher. Permite jugar versiones desde la 1.8 hasta la 1.21.5 lo que lo hace una eleccion indudable para el pvp moderno de la 1.9",
             "url": "https://www.lunarclient.com/",
             "tipoUrl": "externa",
             "imagen": "Resorce/img/lunar.jpg",
-            "pagina": "publicaciones/clientes-lunar-cleint-1.html"
+            "pagina": "publicaciones/clientes-lunar-client-1.html"
         }
     ],
     "especial": []
